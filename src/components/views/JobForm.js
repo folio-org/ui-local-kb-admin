0959d09030fe4bf01121131a0eb6317c83ef4d03
EthanFreestone <ethan.freestone@k-int.co.uk>
2020-02-19 15:31:52 +0000

    added new file to render KBART specific fields

--- src/components/views/JobForm.js
+++ src/components/views/JobForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import SafeHTMLMessage from '@folio/react-intl-safe-html';
 import { Field } from 'react-final-form';
 import { FileUploaderField } from '@folio/stripes-erm-components';
 import { AppIcon, TitleManager } from '@folio/stripes/core';
@@ -9,19 +8,14 @@ import stripesFinalForm from '@folio/stripes/final-form';
 
 import {
   Button,
-  Col,
   IconButton,
-  MessageBanner,
   Pane,
   PaneFooter,
   PaneMenu,
   Paneset,
-  Row,
-  TextField,
 } from '@folio/stripes/components';
 
-import { required } from '../../util/validators';
-
+import KbartFields from './KbartFields';
 import css from './JobForm.css';
 
 class JobForm extends React.Component {
@@ -111,59 +105,8 @@ class JobForm extends React.Component {
                 <form>
                   <div className={css.jobForm}>
                     {format === 'KBART' &&
-                      <>
-                        <Row>
-                          <Col xs={12}>
-                            <MessageBanner>
-                              <SafeHTMLMessage id="ui-local-kb-admin.job.sourceReferenceWarning" />
-                            </MessageBanner>
-                          </Col>
-                        </Row>
-                        <Row>
-                          <Col xs={12}>
-                            <Field
-                              data-test-field-package-name
-                              component={TextField}
-                              name="packageName"
-                              label={<FormattedMessage id="ui-local-kb-admin.job.packageName" />}
-                              required
-                              validate={required}
-                            />
-                          </Col>
-                        </Row>
-                        <Row>
-                          <Col xs={4}>
-                            <Field
-                              data-test-field-package-source
-                              component={TextField}
-                              name="packageSource"
-                              label={<FormattedMessage id="ui-local-kb-admin.job.packageSource" />}
-                              required
-                              validate={required}
-                            />
-                          </Col>
-                          <Col xs={8}>
-                            <Field
-                              data-test-field-package-reference
-                              component={TextField}
-                              name="packageReference"
-                              label={<FormattedMessage id="ui-local-kb-admin.job.packageReference" />}
-                              required
-                              validate={required}
-                            />
-                          </Col>
-                        </Row>
-                        <Row>
-                          <Col xs={12}>
-                            <Field
-                              data-test-field-package-provider
-                              component={TextField}
-                              name="packageProvider"
-                              label={<FormattedMessage id="ui-local-kb-admin.job.packageProvider" />}
-                            />
-                          </Col>
-                        </Row>
-                      </> }
+                      <KbartFields />
+                       }
                     <Field
                       component={FileUploaderField}
                       data-test-document-field-file
