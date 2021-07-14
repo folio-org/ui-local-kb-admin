5e7370b07e3cebd21fc4b5d2b74b2c667f893913
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2021-07-14 16:30:05 +0100

    swapped to new Registry Link API (#200)

--- src/setUpRegistry.js
+++ src/setUpRegistry.js
@@ -1,10 +1,8 @@
-import React from 'react';
-
 const setUpRegistry = (registry) => {
   // Jobs Resource
   const jobReg = registry.registerResource('job');
-  jobReg.addViewAll('/local-kb-admin');
-  jobReg.addViewTemplate(job => `/local-kb-admin/${job.id}`);
+  jobReg.setViewResources('/local-kb-admin');
+  jobReg.setViewResource(job => `/local-kb-admin/${job.id}`);
 };
 
 export default setUpRegistry;
