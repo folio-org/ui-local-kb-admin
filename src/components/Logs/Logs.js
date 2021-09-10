383fa7eb1e73ca29af3173ba2678e03f8dc25070
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-09-10 15:05:32 +0100

    refactor: useCallback
    Wrapped renderBadgeAndExport in useCallback

--- src/components/Logs/Logs.js
+++ src/components/Logs/Logs.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import { Accordion, Badge, Button, Spinner } from '@folio/stripes/components';
@@ -16,7 +16,8 @@ const Logs = ({ id, job, type }) => {
   const logCount = job[`${type}LogCount`];
 
   const { refetch: fetchLogs, isLoading } = useExportLogStream(job, type);
-  const renderBadgeAndExport = () => {
+
+  const renderBadgeAndExport = useCallback(() => {
     return (
       <>
         <Button
@@ -34,7 +35,7 @@ const Logs = ({ id, job, type }) => {
         </Badge>
       </>
     );
-  };
+  }, [fetchLogs, isLoading, logCount]);
 
   return (
     <Accordion
