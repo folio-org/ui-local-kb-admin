73c0080e13ceba9a85c2aeec2f3a945f85c08822
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-09-10 15:42:37 +0100

    refactor: Refactoring
    Moved useExportLogStream, added README, removed unnecessary PropTypes from JobViewRoute

--- src/components/Logs/Logs.js
+++ src/components/Logs/Logs.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { Accordion, Badge, Button, Spinner } from '@folio/stripes/components';
 
 import JobLogContainer from '../../containers/JobLogContainer';
-import useExportLogStream from '../../routes/components/useExportLogStream';
+import { useExportLogStream } from '../../hooks';
 
 const propTypes = {
   id: PropTypes.string,
---
+++ src/hooks/index.js
@@ -0,0 +1,1 @@
+export { default as useExportLogStream } from './useExportLogStream';
