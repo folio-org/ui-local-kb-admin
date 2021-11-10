be7376a3cb9bd588c46bd870fcd67757e7177b32
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2021-11-10 10:33:35 +0100

    ERM-1760 Apply keyboard shortcuts modal guidelines
    * use new utility functions and coreShortcuts from stripes-components

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import {
   NavListItem,
   NavListSection,
   checkScope,
+  importShortcuts,
 } from '@folio/stripes/components';
 
 import JobCreateRoute from './routes/JobCreateRoute';
@@ -56,6 +57,8 @@ const App = (props) => {
     }
   };
 
+  const appSpecificShortcuts = importShortcuts(['save', 'expandAllSections', 'collapseAllSections', 'expandOrCollapseAccordion', 'search']);
+
   const shortcuts = [
     {
       name: 'search',
@@ -63,7 +66,7 @@ const App = (props) => {
     },
     {
       name: 'openShortcutModal',
-      handler: setShowKeyboardShortcutsModal
+      handler: () => setShowKeyboardShortcutsModal(true)
     },
   ];
 
@@ -76,7 +79,7 @@ const App = (props) => {
 
   return (
     <>
-      <CommandList commands={defaultKeyboardShortcuts}>
+      <CommandList commands={appSpecificShortcuts}>
         <HasCommand
           commands={shortcuts}
           isWithinScope={checkScope}
@@ -109,7 +112,7 @@ const App = (props) => {
       </CommandList>
       {showKeyboardShortcutsModal && (
         <KeyboardShortcutsModal
-          // allCommands={defaultKeyboardShortcuts}
+          allCommands={appSpecificShortcuts}
           onClose={() => { setShowKeyboardShortcutsModal(false); }}
           open
         />
