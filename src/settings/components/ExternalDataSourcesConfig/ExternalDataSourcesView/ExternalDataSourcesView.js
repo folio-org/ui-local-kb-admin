005a0deebc75f4ff6bf927442a232fec8234ee23
CalamityC <42971992+CalamityC@users.noreply.github.com>
2021-12-15 13:04:36 +0100

    ERM-1949 Reset cursor for remote KB / ERM-1950 Reset syncStatus for remote KB (#226)
    * reset values in ExternalDataSources settings, refs ERM-1949, ERM-1950
    
    * import ActionMenu from stripes-erm-components
    * add getActionMenu, handlers for reset values
    * add Modal
    
    * disable reset options conditionally, refs ERM-1949, ERM-1950
    
    * fix test for ExternalDataSourcesView, refs ERM-1949, ERM-1950
    
    * extend test for ExternalDataSourcesView, refs ERM-1949, ERM-1950
    
    * fix division, refs ERM-1949, ERM-1950
    
    * use moment() instead of Date() for time difference calculation, refs ERM-1949, ERM-1950
    
    Co-authored-by: EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>

--- src/settings/components/ExternalDataSourcesConfig/ExternalDataSourcesView/ExternalDataSourcesView.js
+++ src/settings/components/ExternalDataSourcesConfig/ExternalDataSourcesView/ExternalDataSourcesView.js
@@ -1,140 +1,222 @@
-import React from 'react';
+import React, { useState } from 'react';
+import moment from 'moment';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { Button, Card, Col, KeyValue, Layout, NoValue, Row } from '@folio/stripes/components';
-import { CustomMetaSection } from '@folio/stripes-erm-components';
+import { Button, Card, Col, Icon, KeyValue, Layout, Modal, ModalFooter, NoValue, Row } from '@folio/stripes/components';
+import { ActionMenu, CustomMetaSection } from '@folio/stripes-erm-components';
 import useDisplayMetaInfo from '../useDisplayMetaInfo';
 
 const ExternalDataSourcesView = ({
   input: { value = {} },
   onDelete,
-  onEdit
+  onEdit,
+  onSave
 }) => {
   const { syncStatus, cursor, lastChecked } = useDisplayMetaInfo(value);
+  const [showConfirmResetSyncStatus, setShowConfirmResetSyncStatus] = useState(false);
+
+  const hours = moment.utc().diff(moment.utc(value.lastCheck), 'hours');
+  const messageType = hours >= 24 ? 'active' : 'passive';
+
+  const handleResetCursor = () => {
+    value.cursor = null;
+    onSave();
+  };
+
+  const renderModal = () => {
+    const footer = (
+      <ModalFooter>
+        {messageType === 'active' && (
+          <Button
+            buttonStyle="danger"
+            data-test-confirm-modal
+            onClick={() => {
+              value.syncStatus = 'idle';
+              onSave();
+              setShowConfirmResetSyncStatus(false);
+            }}
+          >
+            <FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.confirmLabel" />
+          </Button>
+        )}
+        <Button
+          data-test-cancel-modal
+          onClick={() => setShowConfirmResetSyncStatus(false)}
+        >
+          <FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.cancelLabel" />
+        </Button>
+      </ModalFooter>
+    );
+
+    return (
+      <Modal
+        enforceFocus={false}
+        footer={footer}
+        id="reset-syncstatus-confirmation"
+        label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.resetSyncStatus" />}
+        open
+        size="medium"
+      >
+        <FormattedMessage id={`ui-local-kb-admin.settings.externalDataSources.confirmMessage.${messageType}`} values={{ hours }} />
+      </Modal>
+    );
+  };
+
+  const getActionMenu = () => (
+    <>
+      <Button
+        buttonStyle="dropdownItem"
+        data-test-external-data-source-edit
+        marginBottom0
+        onClick={onEdit}
+      >
+        <Icon icon="edit">
+          <FormattedMessage id="stripes-core.button.edit" />
+        </Icon>
+      </Button>
+      <Button
+        buttonStyle="dropdownItem"
+        data-test-external-data-source-resetcursor
+        disabled={!value.cursor}
+        marginBottom0
+        onClick={handleResetCursor}
+      >
+        <Icon icon="refresh">
+          <FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.resetCursor" />
+        </Icon>
+      </Button>
+      <Button
+        buttonStyle="dropdownItem"
+        data-test-external-data-source-resetsyncstatus
+        disabled={value.syncStatus === 'idle'}
+        marginBottom0
+        onClick={() => setShowConfirmResetSyncStatus(true)}
+      >
+        <Icon icon="refresh">
+          <FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.resetSyncStatus" />
+        </Icon>
+      </Button>
+      {!value.readonly &&
+        <Button
+          buttonStyle="dropdownItem"
+          data-test-external-data-source-delete
+          marginBottom0
+          onClick={onDelete}
+        >
+          <Icon icon="trash">
+            <FormattedMessage id="stripes-core.button.delete" />
+          </Icon>
+        </Button>
+      }
+    </>
+  );
 
   return (
-    <Card
-      data-test-external-data-source-view
-      headerEnd={(
-        <span>
-          {!value.readonly &&
-            <Button
-              buttonStyle="danger"
-              data-test-external-data-source-delete
-              marginBottom0
-              onClick={onDelete}
-            >
-              <FormattedMessage id="stripes-core.button.delete" />
-            </Button>
-          }
-          <Button
-            data-test-external-data-source-edit
-            marginBottom0
-            onClick={onEdit}
-          >
-            <FormattedMessage id="stripes-core.button.edit" />
-          </Button>
-        </span>
-      )}
-      headerStart={<strong><FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.externalDataSource" /></strong>}
-    >
-      <CustomMetaSection accordionLabel={syncStatus}>
-        {cursor}
-        {lastChecked}
-      </CustomMetaSection>
-      <Row>
-        <Col xs={3}>
-          <KeyValue
-            data-test-external-data-source-name
-            label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.name" />}
-            value={value.name}
-          />
-        </Col>
-        <Col xs={5}>
-          <KeyValue
-            data-test-external-data-source-type
-            label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.type" />}
-            value={value.type}
-          />
-        </Col>
-        <Col xs={4}>
-          <KeyValue
-            data-test-external-data-source-recordtype
-            label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.recordType" />}
-            value={value.rectype === 1 ? <FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.package" /> : ''}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={8}>
-          <KeyValue
-            data-test-external-data-source-uri
-            label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.uri" />}
-            value={value?.uri ?? <NoValue />}
-          />
-        </Col>
-        <Col xs={4}>
-          <KeyValue
-            data-test-external-data-source-trusted-source-ti
-            label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.trustedSourceTI" />}
-            value={<FormattedMessage id={value.trustedSourceTI ? 'ui-local-kb-admin.yes' : 'ui-local-kb-admin.no'} />}
-          />
-        </Col>
-      </Row>
-      <Layout className="padding-bottom-gutter">
+    <>
+      <Card
+        data-test-external-data-source-view
+        headerEnd={<ActionMenu actionMenu={getActionMenu} />}
+        headerStart={<strong><FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.externalDataSource" /></strong>}
+      >
+        {showConfirmResetSyncStatus && renderModal()}
+        <CustomMetaSection accordionLabel={syncStatus}>
+          {cursor}
+          {lastChecked}
+        </CustomMetaSection>
         <Row>
           <Col xs={3}>
             <KeyValue
-              data-test-external-data-source-isactive
-              label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.isActive" />}
-              value={<FormattedMessage id={value.active ? 'ui-local-kb-admin.yes' : 'ui-local-kb-admin.no'} />}
+              data-test-external-data-source-name
+              label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.name" />}
+              value={value.name}
             />
           </Col>
           <Col xs={5}>
             <KeyValue
-              data-test-external-data-source-supportsharvesting
-              label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.supportsHarvesting" />}
-              value={<FormattedMessage id={value.supportsHarvesting ? 'ui-local-kb-admin.yes' : 'ui-local-kb-admin.no'} />}
+              data-test-external-data-source-type
+              label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.type" />}
+              value={value.type}
             />
           </Col>
           <Col xs={4}>
             <KeyValue
-              data-test-external-data-source-activationenabled
-              label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.activationEnabled" />}
-              value={<FormattedMessage id={value.activationEnabled ? 'ui-local-kb-admin.yes' : 'ui-local-kb-admin.no'} />}
+              data-test-external-data-source-recordtype
+              label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.recordType" />}
+              value={value.rectype === 1 ? <FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.package" /> : ''}
             />
           </Col>
         </Row>
-      </Layout>
-      <Row>
-        <Col xs={3}>
-          <KeyValue
-            data-test-external-data-source-listprefix
-            label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.listPrefix" />}
-            value={value?.listPrefix ?? <NoValue />}
-          />
-        </Col>
-        <Col xs={5}>
-          <KeyValue
-            data-test-external-data-source-fullprefix
-            label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.fullPrefix" />}
-            value={value?.fullPrefix ?? <NoValue />}
-          />
-        </Col>
-        <Col xs={4}>
-          <KeyValue
-            data-test-external-data-source-principal
-            label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.principal" />}
-            value={value?.principal ?? <NoValue />}
-          />
-        </Col>
-      </Row>
-      <KeyValue
-        data-test-external-data-source-credentials
-        label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.credentials" />}
-        value={value?.credentials ?? <NoValue />}
-      />
-    </Card>
+        <Row>
+          <Col xs={8}>
+            <KeyValue
+              data-test-external-data-source-uri
+              label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.uri" />}
+              value={value?.uri ?? <NoValue />}
+            />
+          </Col>
+          <Col xs={4}>
+            <KeyValue
+              data-test-external-data-source-trusted-source-ti
+              label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.trustedSourceTI" />}
+              value={<FormattedMessage id={value.trustedSourceTI ? 'ui-local-kb-admin.yes' : 'ui-local-kb-admin.no'} />}
+            />
+          </Col>
+        </Row>
+        <Layout className="padding-bottom-gutter">
+          <Row>
+            <Col xs={3}>
+              <KeyValue
+                data-test-external-data-source-isactive
+                label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.isActive" />}
+                value={<FormattedMessage id={value.active ? 'ui-local-kb-admin.yes' : 'ui-local-kb-admin.no'} />}
+              />
+            </Col>
+            <Col xs={5}>
+              <KeyValue
+                data-test-external-data-source-supportsharvesting
+                label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.supportsHarvesting" />}
+                value={<FormattedMessage id={value.supportsHarvesting ? 'ui-local-kb-admin.yes' : 'ui-local-kb-admin.no'} />}
+              />
+            </Col>
+            <Col xs={4}>
+              <KeyValue
+                data-test-external-data-source-activationenabled
+                label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.activationEnabled" />}
+                value={<FormattedMessage id={value.activationEnabled ? 'ui-local-kb-admin.yes' : 'ui-local-kb-admin.no'} />}
+              />
+            </Col>
+          </Row>
+        </Layout>
+        <Row>
+          <Col xs={3}>
+            <KeyValue
+              data-test-external-data-source-listprefix
+              label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.listPrefix" />}
+              value={value?.listPrefix ?? <NoValue />}
+            />
+          </Col>
+          <Col xs={5}>
+            <KeyValue
+              data-test-external-data-source-fullprefix
+              label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.fullPrefix" />}
+              value={value?.fullPrefix ?? <NoValue />}
+            />
+          </Col>
+          <Col xs={4}>
+            <KeyValue
+              data-test-external-data-source-principal
+              label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.principal" />}
+              value={value?.principal ?? <NoValue />}
+            />
+          </Col>
+        </Row>
+        <KeyValue
+          data-test-external-data-source-credentials
+          label={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.credentials" />}
+          value={value?.credentials ?? <NoValue />}
+        />
+      </Card>
+    </>
   );
 };
 
@@ -168,6 +250,7 @@ ExternalDataSourcesView.propTypes = {
   }),
   onDelete: PropTypes.func.isRequired,
   onEdit: PropTypes.func.isRequired,
+  onSave: PropTypes.func.isRequired
 };
 
 export default ExternalDataSourcesView;
