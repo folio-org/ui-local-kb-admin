b3355d760eb1b3a89084fc43181a9d2d144c6134
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-12-08 13:58:17 +0000

    refactor: ProxyServerSettingsRoute
    Refactored ProxyServerSettingsRoute to functional/react-query
    
    ERM-2516

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -4,5 +4,7 @@ export const JSON_IMPORT_ENDPOINT = `${JOBS_BASE_ENDPOINT}/packageImport`;
 export const KBART_IMPORT_ENDPOINT = `${JOBS_BASE_ENDPOINT}/kbartImport`;
 
 export const KBS_ENDPOINT = 'erm/kbs';
+export const STS_ENDPOINT = 'erm/sts';
+export const PLATFORMS_ENDPOINT = 'erm/platforms';
 
 export const REFDATA_ENDPOINT = 'erm/refdata';
