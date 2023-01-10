8b0b17bd1c6a56e477633f7b3a9d1688419ee327
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2023-01-10 16:12:50 +0000

    ERM-2528, Add test coverage for ProxyServerSettingsView

---
+++ src/settings/components/ProxyServerSettingsConfig/ProxyServerSettingsView/ProxyServerSettingsView.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { renderWithIntl, TestForm } from '@folio/stripes-erm-testing';
+import { Button, KeyValue } from '@folio/stripes-testing';
+
+import translationsProperties from '../../../../../test/helpers';
+import { input, meta, mutators, platforms } from './testResources';
+import ProxyServerSettingsView from './ProxyServerSettingsView';
+
+const onDelete = jest.fn();
+const onEdit = jest.fn();
+const onSubmit = jest.fn();
+
+describe('ProxyServerSettingsView', () => {
+  let renderComponent;
+  beforeEach(() => {
+    renderComponent = renderWithIntl(
+      <TestForm onSubmit={onSubmit}>
+        <ProxyServerSettingsView
+          input={input}
+          meta={meta}
+          mutators={mutators}
+          onDelete={onDelete}
+          onEdit={onEdit}
+          platforms={platforms}
+        />
+      </TestForm>,
+      translationsProperties
+    );
+  });
+
+  test('renders the Proxy server setting header', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('Proxy server setting')).toBeInTheDocument();
+  });
+
+  test('renders the Delete button', async () => {
+    await Button('Delete').exists();
+  });
+
+  test('renders the Edit button', async () => {
+    await Button('Edit').exists();
+  });
+
+  test('renders the Name keyValue with expected value', async () => {
+    await KeyValue('Name').has({ value: 'test' });
+  });
+
+  test('renders the URL customization code keyValue with expected value', async () => {
+    await KeyValue('URL customization code').has({ value: 'http://localhost:3000/settings/local-kb-admin/proxy-server-settings' });
+  });
+
+  test('renders the Platforms to exclude from proxy server setting keyValue with expected value', async () => {
+    await KeyValue('Platforms to exclude from proxy server setting').has({ value: 'Oxford Academic Journals' });
+  });
+});
---
+++ src/settings/components/ProxyServerSettingsConfig/ProxyServerSettingsView/testResources.js
@@ -0,0 +1,61 @@
+const input = {
+  'name': 'stringTemplates[0]',
+  'value': {
+    'id': 'b03c2a7e-5ed3-4d58-8cdc-7e8c3fdf9d74',
+    'dateCreated': '2023-01-10T14:01:36Z',
+    'rule': 'http://localhost:3000/settings/local-kb-admin/proxy-server-settings',
+    'context': {
+      'id': '2c91809c859beb1f01859bf328ab0047',
+      'value': 'urlproxier',
+      'label': 'urlProxier'
+    },
+    'lastUpdated': '2023-01-10T15:59:02Z',
+    'name': 'test',
+    'idScopes': [{
+      'label': 'Oxford Academic Journals',
+      'value': '64d8acbf-5964-4223-bc5c-83c3659ef843'
+    }]
+  },
+  'onBlur': 'ƒ () {}',
+  'onChange': 'ƒ () {}',
+  'onFocus': 'ƒ () {}'
+};
+
+const meta = {
+  'active': false,
+  'data': '{}',
+  'dirty': false,
+  'dirtySinceLastSubmit': false,
+  'initial': {
+    'id': 'b03c2a7e-5ed3-4d58-8cdc-7e8c3fdf9d74',
+    'dateCreated': '2023-01-10T14:01:36Z',
+    'rule': 'http://localhost:3000/settings/local-kb-admin/proxy-server-settings',
+    'context': {
+      'id': '2c91809c859beb1f01859bf328ab0047',
+      'value': 'urlproxier',
+      'label': 'urlProxier'
+    },
+    'lastUpdated': '2023-01-10T15:59:02Z',
+    'name': 'test',
+    'idScopes': [{
+      'label': 'Oxford Academic Journals',
+      'value': '64d8acbf-5964-4223-bc5c-83c3659ef843'
+    }]
+  },
+  'invalid': false,
+  'modified': false,
+  'modifiedSinceLastSubmit': false,
+  'pristine': true,
+  'submitFailed': false,
+  'submitSucceeded': false,
+  'submitting': false,
+  'touched': false,
+  'valid': true,
+  'validating': false,
+  'visited': false
+};
+
+export {
+  input,
+  meta
+};
