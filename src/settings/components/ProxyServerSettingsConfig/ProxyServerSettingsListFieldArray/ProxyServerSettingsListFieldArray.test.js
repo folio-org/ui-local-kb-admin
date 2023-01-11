53abce0dc28f4f928fa44dadc85285468730db6d
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2023-01-11 20:35:09 +0000

    lint

--- src/settings/components/ProxyServerSettingsConfig/ProxyServerSettingsListFieldArray/ProxyServerSettingsListFieldArray.test.js
+++ src/settings/components/ProxyServerSettingsConfig/ProxyServerSettingsListFieldArray/ProxyServerSettingsListFieldArray.test.js
@@ -12,23 +12,22 @@ const onSubmit = jest.fn();
 jest.mock('../ProxyServerSettingsFields', () => () => <div>ProxyServerSettingsFields</div>);
 
 const initialValues = {
-  "stringTemplates": [{
-      "id": "85d11efb-fef5-4960-a509-0fc72b8c1ff3",
-      "dateCreated": "2023-01-11T17:54:34Z",
-      "rule": "https://www.test.co.uk/",
-      "context": {
-        "id": "2c91809c85a1216c0185a1297fbb0039",
-        "value": "urlproxier",
-        "label": "urlProxier"
-      },
-      "lastUpdated": "2023-01-11T17:54:34Z",
-      "name": "MR test",
-      "idScopes": [{
-        "label": "DeGruyter Online",
-        "value": "1d3050af-762c-4132-afa5-dfbd57a27766"
-      }]
-    }
-  ]
+  'stringTemplates': [{
+    'id': '85d11efb-fef5-4960-a509-0fc72b8c1ff3',
+    'dateCreated': '2023-01-11T17:54:34Z',
+    'rule': 'https://www.test.co.uk/',
+    'context': {
+      'id': '2c91809c85a1216c0185a1297fbb0039',
+      'value': 'urlproxier',
+      'label': 'urlProxier'
+    },
+    'lastUpdated': '2023-01-11T17:54:34Z',
+    'name': 'MR test',
+    'idScopes': [{
+      'label': 'DeGruyter Online',
+      'value': '1d3050af-762c-4132-afa5-dfbd57a27766'
+    }]
+  }]
 };
 
 describe('ProxyServerSettingsListFieldArray', () => {
