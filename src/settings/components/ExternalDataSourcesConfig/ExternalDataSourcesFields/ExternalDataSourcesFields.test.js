21b4c04fc7fc52494b727eaab7d9edd463f5a819
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2023-01-11 19:17:07 +0000

    update tests

--- src/settings/components/ExternalDataSourcesConfig/ExternalDataSourcesFields/ExternalDataSourcesFields.test.js
+++ src/settings/components/ExternalDataSourcesConfig/ExternalDataSourcesFields/ExternalDataSourcesFields.test.js
@@ -1,30 +1,51 @@
 import React from 'react';
+import { Field } from 'react-final-form';
 
-import { renderWithIntl } from '@folio/stripes-erm-testing';
-import { MemoryRouter } from 'react-router-dom';
+import { TestForm, renderWithIntl } from '@folio/stripes-erm-testing';
 
 import translationsProperties from '../../../../../test/helpers';
-import { input, mutators } from './testResources';
 import ExternalDataSourcesFields from './ExternalDataSourcesFields';
 
+const onSubmit = jest.fn();
 const onDeleteMock = jest.fn();
 const onSaveMock = jest.fn();
 
 jest.mock('../ExternalDataSourcesView', () => () => <div>ExternalDataSourcesView</div>);
 
-let renderComponent;
+const initialValues = {
+  'externalKbs': [{
+    'id': 'c4c903d0-f7ef-4d84-8fc7-2ed738e21b91',
+    'cursor': '2023-01-11T14:47:18Z',
+    'active': true,
+    'trustedSourceTI': false,
+    'activationEnabled': false,
+    'readonly': false,
+    'syncStatus': 'idle',
+    'lastCheck': 1673463680170,
+    'name': 'GOKb_TEST',
+    'type': 'org.olf.kb.adapters.GOKbOAIAdapter',
+    'fullPrefix': 'gokb',
+    'uri': 'https://gokbt.gbv.de/gokb/oai/index',
+    'supportsHarvesting': true,
+    'rectype': 1
+  }]
+};
 
 describe('ExternalDataSourcesFields', () => {
+  let renderComponent;
   beforeEach(() => {
     renderComponent = renderWithIntl(
-      <MemoryRouter>
-        <ExternalDataSourcesFields
-          input={input}
-          mutators={mutators}
+      <TestForm
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+      >
+        <Field
+          component={ExternalDataSourcesFields}
+          name="externalKbs[0]"
           onDelete={onDeleteMock}
           onSave={onSaveMock}
         />
-      </MemoryRouter>,
+      </TestForm>,
       translationsProperties
     );
   });
