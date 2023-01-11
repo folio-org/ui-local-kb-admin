664f76d6f6a3aba5c64e3476a0f04c64c97d71db
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2023-01-11 16:13:51 +0000

    tweak

--- src/settings/components/ProxyServerSettingsConfig/ProxyServerSettingsFields/ProxyServerSettingsFields.test.js
+++ src/settings/components/ProxyServerSettingsConfig/ProxyServerSettingsFields/ProxyServerSettingsFields.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Field } from 'react-final-form';
 import { renderWithIntl, TestForm } from '@folio/stripes-erm-testing';
 
 import translationsProperties from '../../../../../test/helpers';
@@ -10,52 +11,60 @@ const onSubmit = jest.fn();
 
 jest.mock('../ProxyServerSettingsView', () => () => <div>ProxyServerSettingsView</div>);
 
-const input = {
-  'name': 'stringTemplates[1]',
-  'value': {
-    'id': 'b03c2a7e-5ed3-4d58-8cdc-7e8c3fdf9d74',
-    'dateCreated': '2023-01-10T14:01:36Z',
-    'rule': 'http://localhost:3000/settings/local-kb-admin/proxy-server-settings',
+const initialValues = {
+  'stringTemplates': [{
+    'id': '7e273329-0cbf-498b-8ed8-4c4b029670bc',
+    'dateCreated': '2023-01-11T14:36:36Z',
+    'rule': 'https://www.google.co.uk/',
     'context': {
-      'id': '2c91809c859beb1f01859bf328ab0047',
+      'id': '2c91809c85a1216c0185a1297fbb0039',
       'value': 'urlproxier',
       'label': 'urlProxier'
     },
-    'lastUpdated': '2023-01-10T15:59:02Z',
-    'name': 'test',
+    'lastUpdated': '2023-01-11T14:36:36Z',
+    'name': 'MR test',
     'idScopes': [{
       'label': 'Oxford Academic Journals',
-      'value': '64d8acbf-5964-4223-bc5c-83c3659ef843'
-    }]
+      'value': '61230f51-9703-4734-9232-b6d11c3a2598'
+    },
+    {
+      'label': 'DeGruyter Online',
+      'value': '1d3050af-762c-4132-afa5-dfbd57a27766'
+    }
+    ]
   },
-  'onBlur': 'ƒ () {}',
-  'onChange': 'ƒ () {}',
-  'onFocus': 'ƒ () {}'
-};
-
-const mutators = {
-  'setProxyServerSettingValue': 'ƒ () {}',
-  'insert': 'ƒ () {}',
-  'concat': 'ƒ () {}',
-  'move': 'ƒ () {}',
-  'pop': 'ƒ () {}',
-  'push': 'ƒ () {}',
-  'remove': 'ƒ () {}',
-  'removeBatch': 'ƒ () {}',
-  'shift': 'ƒ () {}',
-  'swap': 'ƒ () {}',
-  'unshift': 'ƒ () {}',
-  'update': 'ƒ () {}'
+  {
+    'id': '4296951d-b7e1-4710-aee6-27c0a5e89e94',
+    'dateCreated': '2023-01-11T16:06:47Z',
+    'rule': 'https://www.test.co.uk/',
+    'context': {
+      'id': '2c91809c85a1216c0185a1297fbb0039',
+      'value': 'urlproxier',
+      'label': 'urlProxier'
+    },
+    'lastUpdated': '2023-01-11T16:06:47Z',
+    'name': 'MR test',
+    'idScopes': [{
+      'label': 'Sage',
+      'value': '3945d877-4e89-406b-88a0-959cdc43aebf'
+    },
+    {
+      'label': 'Oxford Academic Journals',
+      'value': '61230f51-9703-4734-9232-b6d11c3a2598'
+    }
+    ]
+  }
+  ]
 };
 
 describe('ProxyServerSettingsFields', () => {
   let renderComponent;
   beforeEach(() => {
     renderComponent = renderWithIntl(
-      <TestForm onSubmit={onSubmit}>
-        <ProxyServerSettingsFields
-          input={input}
-          mutators={mutators}
+      <TestForm initialValues={initialValues} onSubmit={onSubmit}>
+        <Field
+          component={ProxyServerSettingsFields}
+          name="stringTemplates[0]"
           onDelete={onDelete}
           onSave={onSave}
         />
