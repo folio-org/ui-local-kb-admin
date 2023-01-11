ea2f8f4f443471a292fd3e5e12e5aa3404e1166b
CalamityC <42971992+CalamityC@users.noreply.github.com>
2023-01-11 13:45:07 +0100

    ERM-1515 Add test coverage for ui-local-kb-admin JobsRoute (#299)
    * ERM-1515 Add test coverage for ui-local-kb-admin JobsRoute
    
    * move component to directory
    * add test
    * add data-testid to Jobs component
    * create and use refdata mock file
    
    * fix sonarcloud code smell, refs ERM-1515
    
    * Revert "fix sonarcloud code smell, refs ERM-1515"
    because it made things worse
    
    This reverts commit 80e6417d3124309b492b83eecc6e5d0f0b3ebecc.
    
    Co-authored-by: Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>

---
+++ src/routes/JobsRoute/JobsRoute.test.js
@@ -0,0 +1,57 @@
+
+import { renderWithIntl } from '@folio/stripes-erm-testing';
+import { MemoryRouter } from 'react-router-dom';
+import translationsProperties from '../../../test/helpers/translationsProperties';
+import JobsRoute from './JobsRoute';
+import mockRefdata from '../../../test/jest/refdata';
+
+jest.mock('../../hooks', () => ({
+  ...jest.requireActual('../../hooks'),
+  useLocalKBAdminRefdata: () => mockRefdata,
+}));
+
+const routeProps = {
+  history: {
+    push: () => jest.fn()
+  },
+  location: {},
+  match: {
+    params: {},
+  },
+};
+
+describe('JobsRoute', () => {
+  describe('rendering the route', () => {
+    let renderComponent;
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <MemoryRouter>
+          <JobsRoute {...routeProps} />
+        </MemoryRouter>,
+        translationsProperties
+      );
+    });
+
+    test('renders the jobs component', () => {
+      const { getByTestId } = renderComponent;
+      expect(getByTestId('jobs')).toBeInTheDocument();
+    });
+
+    describe('re-rendering the route', () => { // makes sure that we hit the componentDidUpdate block
+      beforeEach(() => {
+        renderWithIntl(
+          <MemoryRouter>
+            <JobsRoute {...routeProps} />
+          </MemoryRouter>,
+          translationsProperties,
+          renderComponent.rerender
+        );
+      });
+
+      test('renders the jobs component', () => {
+        const { getByTestId } = renderComponent;
+        expect(getByTestId('jobs')).toBeInTheDocument();
+      });
+    });
+  });
+});
