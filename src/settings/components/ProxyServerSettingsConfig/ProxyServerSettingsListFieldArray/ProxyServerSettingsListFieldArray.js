08387f3bba2ee14fe160f8f1f4115118ce952d4f
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2023-01-11 18:20:48 +0000

    ERM-2527, Add test coverage for ProxyServerSettingsListFieldArray

--- src/settings/components/ProxyServerSettingsConfig/ProxyServerSettingsListFieldArray/ProxyServerSettingsListFieldArray.js
+++ src/settings/components/ProxyServerSettingsConfig/ProxyServerSettingsListFieldArray/ProxyServerSettingsListFieldArray.js
@@ -4,7 +4,7 @@ import { FormattedMessage } from 'react-intl';
 import { isEqual } from 'lodash';
 import { Button, Col, Row } from '@folio/stripes/components';
 import { Field } from 'react-final-form';
-import ProxyServerSettingsFields from './ProxyServerSettingsFields';
+import ProxyServerSettingsFields from '../ProxyServerSettingsFields';
 
 export default class ProxyServerSettingsListFieldArray extends React.Component {
   static propTypes = {
---
+++ src/settings/components/ProxyServerSettingsConfig/ProxyServerSettingsListFieldArray/ProxyServerSettingsListFieldArray.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { FieldArray } from 'react-final-form-arrays';
+import { renderWithIntl, TestForm } from '@folio/stripes-erm-testing';
+
+import translationsProperties from '../../../../../test/helpers';
+import ProxyServerSettingsListFieldArray from './ProxyServerSettingsListFieldArray';
+
+const onDelete = jest.fn();
+const onSave = jest.fn();
+const onSubmit = jest.fn();
+
+jest.mock('../ProxyServerSettingsFields', () => () => <div>ProxyServerSettingsFields</div>);
+
+const initialValues = {
+  "stringTemplates": [{
+      "id": "85d11efb-fef5-4960-a509-0fc72b8c1ff3",
+      "dateCreated": "2023-01-11T17:54:34Z",
+      "rule": "https://www.test.co.uk/",
+      "context": {
+        "id": "2c91809c85a1216c0185a1297fbb0039",
+        "value": "urlproxier",
+        "label": "urlProxier"
+      },
+      "lastUpdated": "2023-01-11T17:54:34Z",
+      "name": "MR test",
+      "idScopes": [{
+        "label": "DeGruyter Online",
+        "value": "1d3050af-762c-4132-afa5-dfbd57a27766"
+      }]
+    }
+  ]
+};
+
+describe('ProxyServerSettingsListFieldArray', () => {
+  let renderComponent;
+  beforeEach(() => {
+    renderComponent = renderWithIntl(
+      <TestForm initialValues={initialValues} onSubmit={onSubmit}>
+        <FieldArray
+          component={ProxyServerSettingsListFieldArray}
+          name="stringTemplates"
+          onDelete={onDelete}
+          onSave={onSave}
+        />
+      </TestForm>,
+      translationsProperties
+    );
+  });
+
+  test('renders the ProxyServerSettingsFields component', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('ProxyServerSettingsFields')).toBeInTheDocument();
+  });
+});
+
