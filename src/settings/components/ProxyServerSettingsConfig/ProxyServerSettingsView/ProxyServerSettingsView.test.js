a2b157e7d28667e81985402c8e900172618b7cdc
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2023-01-11 17:17:58 +0000

    Removed testResources file and chenged the props

--- src/settings/components/ProxyServerSettingsConfig/ProxyServerSettingsView/ProxyServerSettingsView.test.js
+++ src/settings/components/ProxyServerSettingsConfig/ProxyServerSettingsView/ProxyServerSettingsView.test.js
@@ -1,24 +1,43 @@
 import React from 'react';
+import { Field } from 'react-final-form';
 
 import { renderWithIntl, TestForm } from '@folio/stripes-erm-testing';
 import { Button, KeyValue } from '@folio/stripes-testing';
 
 import translationsProperties from '../../../../../test/helpers';
-import { input, meta } from './testResources';
 import ProxyServerSettingsView from './ProxyServerSettingsView';
 
 const onDelete = jest.fn();
 const onEdit = jest.fn();
 const onSubmit = jest.fn();
 
+const initialValues = {
+  'stringTemplates': [{
+    'id': '5dd1311f-cfd8-4547-852d-cf38dc5fd376',
+    'dateCreated': '2023-01-11T17:14:21Z',
+    'rule': 'http://localhost:3000/settings/local-kb-admin/proxy-server-settings',
+    'context': {
+      'id': '2c91809c85a1216c0185a1297fbb0039',
+      'value': 'urlproxier',
+      'label': 'urlProxier'
+    },
+    'lastUpdated': '2023-01-11T17:14:21Z',
+    'name': 'test',
+    'idScopes': [{
+      'label': 'Oxford Academic Journals',
+      'value': '61230f51-9703-4734-9232-b6d11c3a2598'
+    }]
+  }]
+};
+
 describe('ProxyServerSettingsView', () => {
   let renderComponent;
   beforeEach(() => {
     renderComponent = renderWithIntl(
-      <TestForm onSubmit={onSubmit}>
-        <ProxyServerSettingsView
-          input={input}
-          meta={meta}
+      <TestForm initialValues={initialValues} onSubmit={onSubmit}>
+        <Field
+          component={ProxyServerSettingsView}
+          name="stringTemplates[0]"
           onDelete={onDelete}
           onEdit={onEdit}
         />
