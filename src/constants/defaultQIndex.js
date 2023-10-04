c2cce11bafeb55323a3c0c9d56a7aea2cbdf9bd1
MonireRasouli <monireh.rasouli@k-int.co.uk>
2023-10-04 13:05:08 +0100

    removed unneccessary file

--- src/constants/defaultQIndex.js
+++
@@ -1,4 +0,0 @@
-// // Defined as a constant here because we use it as a fallback for the query AND within the SASQ initial setup
-const defaultLocalKBQIndex = 'name';
-
-export default { defaultLocalKBQIndex };
