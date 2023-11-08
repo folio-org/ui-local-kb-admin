94295dcac68520722ae57408a72a5670cf02bfac
Claudia Malzer <claudia.malzer@tu-darmstadt.de>
2023-11-08 16:13:34 +0100

    ERM-2792 Swap external datasources (LKB) from card-based to MCL based form
    * fix deprecated issues

--- src/settings/components/ExternalDataSourcesSettings/ExternalDataSourcesSettings.js
+++ src/settings/components/ExternalDataSourcesSettings/ExternalDataSourcesSettings.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { useStripes } from '@folio/stripes/core';
-import { Button, Col, Icon, KeyValue, MultiColumnList, NoValue, Pane, Row } from '@folio/stripes/components';
+import { Button, Col, Icon, KeyValue, MultiColumnList, NoValue, Pane, PaneHeader, Row } from '@folio/stripes/components';
 import { CustomMetaSection } from '@folio/stripes-erm-components';
 import useDisplayMetaInfo from './useDisplayMetaInfo';
 import ExternalDataSourcesFormModal from './ExternalDataSourcesFormModal';
@@ -94,26 +94,33 @@ const ExternalDataSourcesSettings = ({
     return (actionsArray?.length ? actionsArray : null);
   };
 
+  const renderHeader = renderProps => (
+    <PaneHeader
+      {...renderProps}
+      lastMenu={perm ?
+        <Button
+          buttonStyle="primary"
+          data-test-external-data-source-new
+          id="clickable-new-external-datasource"
+          marginBottom0
+          // onClick={onSubmit}
+          onClick={() => setMode(CREATING)}
+        >
+          <FormattedMessage id="ui-local-kb-admin.job.new" />
+        </Button> : null
+      }
+      paneSub={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.sourceCount" values={{ count }} />}
+      paneTitle={<FormattedMessage id="ui-local-kb-admin.section.externalDataSources" />}
+    />
+  );
+
   return (
     <>
       <Pane
         data-test-external-data-sources
         defaultWidth="fill"
         id="settings-external-data-sources"
-        lastMenu={perm ?
-          <Button
-            buttonStyle="primary"
-            data-test-external-data-source-new
-            id="clickable-new-external-datasource"
-            marginBottom0
-            // onClick={onSubmit}
-            onClick={() => setMode(CREATING)}
-          >
-            <FormattedMessage id="ui-local-kb-admin.job.new" />
-          </Button> : null
-        }
-        paneSub={<FormattedMessage id="ui-local-kb-admin.settings.externalDataSources.sourceCount" values={{ count }} />}
-        paneTitle={<FormattedMessage id="ui-local-kb-admin.section.externalDataSources" />}
+        renderHeader={renderHeader}
       >
         <MultiColumnList
           columnMapping={{
